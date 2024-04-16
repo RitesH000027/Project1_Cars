9c80ca3099f635c166c79e57cf6b119fc33913dc
Ritesh Gupta <104141297+RitesH000027@users.noreply.github.com>
2024-04-17 02:22:46 +0530

    Update problem5.js

--- src/problem5.js
+++ src/problem5.js
@@ -1,4 +1,5 @@
-function countCarsOlderThanYear(inventory, year = 2000) {
+function countCarsOlderThanYear(inventory, year = 2000) {  
+    // by default year = 2000 if no year is provided
     let count = 0;
     for (let i = 0; i < inventory.length; i++) {
         if (inventory[i].car_year < year) {
